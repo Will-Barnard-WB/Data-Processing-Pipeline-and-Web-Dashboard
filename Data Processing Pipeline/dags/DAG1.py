f9c06ef44bace4740ec2c6f1900743cc80237d19
Will-Barnard-WB <barnardwillliam@outlook.com>
2025-09-02 18:10:54 +0100

    Fix flake8 indentation and blank lines

--- Data Processing Pipeline/dags/DAG1.py
+++ Data Processing Pipeline/dags/DAG1.py
@@ -59,19 +59,18 @@ with DAG(
     )
 
     task3 = SQLExecuteQueryOperator(
-    task_id="fetch_and_store_apple_stock_data",
-    conn_id="postgres_localhost",
-    sql=(
-        "INSERT INTO stock_data (dt, stock_symbol, open_price, close_price) "
-        "VALUES ('{{ts}}', 'AAPL', "
-        "'{{ task_instance.xcom_pull(task_ids=\"fetch_stock_data\", "
-        "key=\"current_open\") }}', "
-        "'{{ task_instance.xcom_pull(task_ids=\"fetch_stock_data\", "
-        "key=\"current_close\") }}'"
-        ");"
-    ),
-)
-
+        task_id="fetch_and_store_apple_stock_data",
+        conn_id="postgres_localhost",
+        sql=(
+            "INSERT INTO stock_data (dt, stock_symbol, open_price, close_price) "
+            "VALUES ('{{ts}}', 'AAPL', "
+            "'{{ task_instance.xcom_pull(task_ids=\"fetch_stock_data\", "
+            "key=\"current_open\") }}', "
+            "'{{ task_instance.xcom_pull(task_ids=\"fetch_stock_data\", "
+            "key=\"current_close\") }}'"
+            ");"
+        ),
+    )
 
     task4 = SQLExecuteQueryOperator(
         task_id="delete_oldest_stock_data",
